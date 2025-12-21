72059de0150a4aaa8dc62b51f854c78a13d6ba68
hmeltaha <hala@hala.com>
2025-12-21 11:36:39 +0300

    adding env

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/hala/Desktop/crypto_project/venv
+set -gx VIRTUAL_ENV /home/hala/Desktop/crypto/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
